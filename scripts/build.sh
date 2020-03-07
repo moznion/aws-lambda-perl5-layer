833ded09dd460ccba08bd4e176a9bc99ff4ec7e1
moznion <moznion@gmail.com>
2020-03-07 13:20:53 +0900

    Include "libcrypt*" and "libcurl*" in the layer

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,9 @@ cd "/opt" || exit 1
 cp "${BASE_DIR}/bootstrap" .
 chmod 755 'bootstrap'
 
+# XXX copy some libs for workaround: https://github.com/moznion/aws-lambda-perl5-layer/issues/6
+cp -R /lib64/libcrypt* ./lib/
+cp -R /usr/lib64/libcurl* ./lib/
+
 zip -r "${BASE_DIR}/lambda-layer-perl-${TAG}.zip" .
 
